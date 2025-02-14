6d4ca8d94cab4aa308edda2319bb0606d8d367ab
rickbroken <daironquebrada@gmail.com>
2025-02-14 18:02:53 -0500

    feature / se agrega fuentes para el proyecto y se importa componente de menu de navegacion

--- frontend-manager-tasks/src/App.tsx
+++ frontend-manager-tasks/src/App.tsx
@@ -1,10 +1,18 @@
 import './App.css'
+import MenuNavigation from './components/MenuNavigation'
+import WebFont from 'webfontloader'
+
+WebFont.load({
+  google: {
+    families: ['Lexend:100,200,300,400,500,600,700,800,900', 'Droid Serif']
+  }
+});
 
 function App() {
 
   return (
-    <main className=''>
-
+    <main className='w-full bg-gray-50'>
+      <MenuNavigation />
     </main>
   )
 }
