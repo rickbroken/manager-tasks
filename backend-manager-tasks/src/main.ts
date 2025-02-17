b73e367e7761ce11b58f280922d6cb050309922d
rickbroken <daironquebrada@gmail.com>
2025-02-17 00:29:38 -0500

    feature / agregando configuracione spara deploy

--- backend-manager-tasks/src/main.ts
+++ backend-manager-tasks/src/main.ts
@@ -7,6 +7,6 @@ async function bootstrap() {
   app.enableCors();
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 8080);
 }
 bootstrap().catch(console.error);
